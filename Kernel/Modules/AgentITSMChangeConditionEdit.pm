58561cfe703e5c78390465decc9c3df60a733f4a
Udo Bretz <udo.bretz@otrs.com>
2010-06-14 18:46:52 +0000

    Added missing semicolon.

--- Kernel/Modules/AgentITSMChangeConditionEdit.pm
+++ Kernel/Modules/AgentITSMChangeConditionEdit.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/Modules/AgentITSMChangeConditionEdit.pm - the OTRS::ITSM::ChangeManagement condition edit module
-# Copyright (C) 2003-2010 OTRS AG, http://otrs.com/
+# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
 # --
-# $Id: AgentITSMChangeConditionEdit.pm,v 1.34 2010-04-27 20:33:50 ub Exp $
+# $Id: AgentITSMChangeConditionEdit.pm,v 1.34.2.1 2010-06-14 18:46:52 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -19,7 +19,7 @@ use Kernel::System::ITSMChange::ITSMCondition;
 use Kernel::System::Valid;
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.34 $) [1];
+$VERSION = qw($Revision: 1.34.2.1 $) [1];
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -1501,7 +1501,7 @@ sub _GetCompareValueFieldType {
         $FieldType = 'Text';
     }
     elsif ( $FieldType eq 'Autocomplete' ) {
-        $FieldType = 'Selection'
+        $FieldType = 'Selection';
     }
 
     return $FieldType;

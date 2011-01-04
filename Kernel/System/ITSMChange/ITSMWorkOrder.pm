9289354bb98561cf5f48de00f2d2ff9038f2f799
Udo Bretz <udo.bretz@otrs.com>
2011-01-04 13:04:35 +0000

    Fixed bug# 6632 - Unittest shows error log message: Argument "\x{35}\x{2c}..." isn't numeric in numeric eq (==).

--- Kernel/System/ITSMChange/ITSMWorkOrder.pm
+++ Kernel/System/ITSMChange/ITSMWorkOrder.pm
@@ -1,8 +1,8 @@
 # --
 # Kernel/System/ITSMChange/ITSMWorkOrder.pm - all workorder functions
-# Copyright (C) 2001-2010 OTRS AG, http://otrs.org/
+# Copyright (C) 2001-2011 OTRS AG, http://otrs.org/
 # --
-# $Id: ITSMWorkOrder.pm,v 1.114.2.3 2010-09-13 11:26:54 bes Exp $
+# $Id: ITSMWorkOrder.pm,v 1.114.2.4 2011-01-04 13:04:35 ub Exp $
 # --
 # This software comes with ABSOLUTELY NO WARRANTY. For details, see
 # the enclosed file COPYING for license information (AGPL). If you
@@ -27,7 +27,7 @@ use Kernel::System::Cache;
 use base qw(Kernel::System::EventHandler);
 
 use vars qw($VERSION);
-$VERSION = qw($Revision: 1.114.2.3 $) [1];
+$VERSION = qw($Revision: 1.114.2.4 $) [1];
 
 =head1 NAME
 
@@ -806,7 +806,7 @@ sub WorkOrderGet {
             next ATTRIBUTE if !$WorkOrderData{$Attribute};
 
             # do not show zero values
-            if ( $WorkOrderData{$Attribute} == 0 ) {
+            if ( $WorkOrderData{$Attribute} eq 0 ) {
                 $WorkOrderData{$Attribute} = '';
                 next ATTRIBUTE;
             }
@@ -2636,7 +2636,7 @@ sub WorkOrderChangeEffortsGet {
             next ATTRIBUTE if !$ChangeEfforts{$Attribute};
 
             # do not show zero values
-            if ( $ChangeEfforts{$Attribute} == 0 ) {
+            if ( $ChangeEfforts{$Attribute} eq 0 ) {
                 $ChangeEfforts{$Attribute} = '';
                 next ATTRIBUTE;
             }
@@ -3416,6 +3416,6 @@ did not receive this file, see L<http://www.gnu.org/licenses/agpl.txt>.
 
 =head1 VERSION
 
-$Revision: 1.114.2.3 $ $Date: 2010-09-13 11:26:54 $
+$Revision: 1.114.2.4 $ $Date: 2011-01-04 13:04:35 $
 
 =cut

9821ff67af7224d627b3195b93f5f5c90f4d922d
s7design <otrs@s7designcreative.com>
2015-06-29 16:28:53 +0200

    port to OM

--- Kernel/Output/HTML/ToolBar/MyCAB.pm
+++ Kernel/Output/HTML/ToolBar/MyCAB.pm
@@ -6,16 +6,12 @@
 # did not receive this file, see http://www.gnu.org/licenses/agpl.txt.
 # --
 
-package Kernel::Output::HTML::ToolBarMyCAB;
+package Kernel::Output::HTML::ToolBar::MyCAB;
 
 use strict;
 use warnings;
 
-use Kernel::System::ITSMChange;
-
-our @ObjectDependencies = (
-    'Kernel::System::Cache',
-);
+our $ObjectManagerDisabled = 1;
 
 sub new {
     my ( $Type, %Param ) = @_;
@@ -24,20 +20,8 @@ sub new {
     my $Self = {};
     bless( $Self, $Type );
 
-    # get needed objects
-    for (
-        qw(ConfigObject LogObject DBObject TicketObject UserObject GroupObject LayoutObject UserID)
-        )
-    {
-        $Self->{$_} = $Param{$_} || die "Got no $_!";
-    }
-
-    # create needed objects
-    $Self->{ChangeObject} = Kernel::System::ITSMChange->new(%Param);
-
-    # get the cache type and TTL (in seconds)
-    $Self->{CacheType} = 'ITSMChangeManagementToolBarMyCAB' . $Self->{UserID};
-    $Self->{CacheTTL}  = $Self->{ConfigObject}->Get('ITSMChange::ToolBar::CacheTTL') * 60;
+    # get UserID param
+    $Self->{UserID} = $Param{UserID} || die "Got no UserID!";
 
     return $Self;
 }
@@ -50,20 +34,26 @@ sub Run {
     my $Group  = 'itsm-change';
     my $Icon   = $Param{Config}->{Icon};
 
+    # get config object
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
     # do not show icon if frontend module is not registered
-    return if !$Self->{ConfigObject}->Get('Frontend::Module')->{$Action};
+    return if !$ConfigObject->Get('Frontend::Module')->{$Action};
 
     # get config of frontend module
-    my $Config = $Self->{ConfigObject}->Get("ITSMChange::Frontend::$Action");
+    my $Config = $ConfigObject->Get("ITSMChange::Frontend::$Action");
+
+    # get group object
+    my $GroupObject = $Kernel::OM->Get('Kernel::System::Group');
 
     # get the group id
-    my $GroupID = $Self->{GroupObject}->GroupLookup( Group => $Group );
+    my $GroupID = $GroupObject->GroupLookup( Group => $Group );
 
     # deny access, when the group is not found
     return if !$GroupID;
 
     # get user groups, where the user has the appropriate privilege
-    my %Groups = $Self->{GroupObject}->GroupMemberList(
+    my %Groups = $GroupObject->GroupMemberList(
         UserID => $Self->{UserID},
         Type   => $Config->{Permission},
         Result => 'HASH',
@@ -84,9 +74,16 @@ sub Run {
             push @ChangeStates, $ChangeState;
         }
 
+        # get the cache type and TTL (in seconds)
+        $Self->{CacheType} = 'ITSMChangeManagementToolBarMyCAB' . $Self->{UserID};
+        $Self->{CacheTTL}  = $ConfigObject->Get('ITSMChange::ToolBar::CacheTTL') * 60;
+
+        # get cache object
+        my $CacheObject = $Kernel::OM->Get('Kernel::System::Cache');
+
         # check cache
         my $CacheKey = join ',', sort @ChangeStates;
-        my $Cache = $Kernel::OM->Get('Kernel::System::Cache')->Get(
+        my $Cache = $CacheObject->Get(
             Type => $Self->{CacheType},
             Key  => $CacheKey,
         );
@@ -97,7 +94,7 @@ sub Run {
         else {
 
             # count the number of viewable changes
-            $Count = $Self->{ChangeObject}->ChangeSearch(
+            $Count = $Kernel::OM->Get('Kernel::System::ITSMChange')->ChangeSearch(
                 CABAgents    => [ $Self->{UserID} ],
                 ChangeStates => \@ChangeStates,
                 Limit        => 1000,
@@ -107,7 +104,7 @@ sub Run {
             );
 
             # set cache
-            $Kernel::OM->Get('Kernel::System::Cache')->Set(
+            $CacheObject->Set(
                 Type  => $Self->{CacheType},
                 Key   => $CacheKey,
                 Value => $Count,
@@ -116,12 +113,15 @@ sub Run {
         }
     }
 
+    # get layout object
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+
     # get ToolBar object parameters
     my $Class = $Param{Config}->{CssClass};
-    my $Text  = $Self->{LayoutObject}->{LanguageObject}->Translate('My CABs');
+    my $Text  = $LayoutObject->{LanguageObject}->Translate('My CABs');
 
     # set ToolBar object
-    my $URL      = $Self->{LayoutObject}->{Baselink};
+    my $URL      = $LayoutObject->{Baselink};
     my $Priority = $Param{Config}->{Priority};
     my %Return;
     if ($Count) {

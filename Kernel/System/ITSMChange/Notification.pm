7a289779d11e0ec2556b63899abea5b6cdef67e5
Manuel Hecht <manuel.hecht@otrs.com>
2018-04-13 23:52:06 +0200

    Tidied.

--- Kernel/System/ITSMChange/Notification.pm
+++ Kernel/System/ITSMChange/Notification.pm
@@ -1146,7 +1146,7 @@ sub NotificationRuleList {
     # fetch IDs
     my @IDs;
     while ( my @Row = $Kernel::OM->Get('Kernel::System::DB')->FetchrowArray() ) {
-        push @IDs, $Row[0],;
+        push @IDs, $Row[0];
     }
 
     # save values in cache
@@ -1227,7 +1227,7 @@ sub NotificationRuleSearch {
     # fetch IDs
     my @IDs;
     while ( my @Row = $Kernel::OM->Get('Kernel::System::DB')->FetchrowArray() ) {
-        push @IDs, $Row[0],;
+        push @IDs, $Row[0];
     }
 
     # save values in cache

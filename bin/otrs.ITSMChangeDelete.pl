cd1f029288731623da7507e661db9cda00ee8eaa
Manuel Hecht <manuel.hecht@otrs.com>
2019-01-02 21:01:17 +0100

    Updated copyright.

--- bin/otrs.ITSMChangeDelete.pl
+++ bin/otrs.ITSMChangeDelete.pl
@@ -1,6 +1,6 @@
 #!/usr/bin/perl
 # --
-# Copyright (C) 2001-2018 OTRS AG, https://otrs.com/
+# Copyright (C) 2001-2019 OTRS AG, https://otrs.com/
 # --
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -42,7 +42,7 @@ $CommonObject{ChangeObject} = $Kernel::OM->Get('Kernel::System::ITSMChange');
 print "\n";
 print "otrs.ITSMChangeDelete.pl\n";
 print "delete changes (all or by number).\n";
-print "Copyright (C) 2001-2018 OTRS AG, https://otrs.com/\n\n";
+print "Copyright (C) 2001-2019 OTRS AG, https://otrs.com/\n\n";
 
 my $Help          = '';
 my $All           = '';
